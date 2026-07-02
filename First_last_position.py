aef2c8694adc912d5a6a3c9b607aeca410984492
Bhavyareddy-15 <bhavyareddy8179@gmail.com>
2026-07-02 23:02:33 +0530

    Create First_last_position.py

---
+++ First_last_position.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        ans=[]
+        for i in range(len(nums)):
+            if nums[i]==target:
+                ans.append(i)
+        if not ans:
+            return [-1,-1]
+        return [ans[0],ans[-1]]
